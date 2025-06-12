7055f6f7286f07e0e0198dc4e336785ccc353f49
MOroushdy <mostafamhmoud83812@gmail.com>
2025-06-13 00:35:53 +0300

    edit a design

--- src/components/Books/sections/AppPromoSection.js
+++ src/components/Books/sections/AppPromoSection.js
@@ -4,26 +4,20 @@ import phoneImage from '../components/assetes/iphone.png'; // تأكد من ال
 import appstore from '../components/assetes/App Store Badge.png';
 import GooglePlay from '../components/assetes/Google Play Badge.png';
 
- // تأكد من المسار الصحيح
-// import appStoreBadge from '../assets/app-store-badge.png'; 
-// تأكد من المسار الصحيح
-// import './AppPromoSection.css';
- // استيراد ملف CSS
-
 function AppPromoSection() {
   return (
-    <section className="app-promo-section"> {/* الأب الرئيسي */}
-      <div className="container app-promo-content"> {/* حاوية المحتوى مع position: relative */}
+    <section className="app-promo-section">
+      <div className="container app-promo-content"> 
 
-        <div className="app-promo-image"> {/* حاوية الهاتف المطلقة */}
+        <div className="app-promo-image"> 
           <img src={phoneImage} alt="Lexi Read App on phone" />
         </div>
 
-        <div className="app-promo-text"> {/* حاوية النص */}
+        <div className="app-promo-text"> 
           <h2>Discover it now on the App Store!</h2>
           <p>Unlock a smarter way to read - Download Lexi Read and dive into your language adventure today!</p>
           <div className="app-store-buttons">
-            <p>Get the App</p> {/* عنوان صغير للأزرار */}
+            <p>Get the App</p> 
             <button>
               <img src= {GooglePlay}alt="Get it on Google Play" />
             </button>
